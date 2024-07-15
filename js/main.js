0ec170e2e2f1bc417b4c670e9516851314779564
Kim Songhee <bokchi8@gmail.com>
2024-07-15 13:20:52 +0900

    쇼핑 섹션에 슬라이드 적용 - 스와이퍼, 정렬 갯수 등

--- js/main.js
+++ js/main.js
@@ -22,11 +22,50 @@ window.onload = function(){
             nextEl: ".promotion .sw-next",
             prevEl: ".promotion .sw-prev",
         },
-        loop : true,
+        pagination:{
+            el:".sw-promotion-pg",
+            clickable: true,
+        },
+        loop: true,
         autoplay: {
             delay: 2500,
             disableOnInteraction: false,
           },
 
+    })
+    // 쇼핑의 스와이퍼 기능 적용
+    const swShopping = new Swiper(".sw-shopping",{
+        slidesPerView: 3,
+        grid:{
+            rows : 3,
+            fill:"row"
+        },
+        spaceBetween: 10,
+        breakpoints:{
+            900 : {
+                spaceBetween: 32,
+                slidesPerView: 4,
+                slidesPerGroup: 3,
+                grid:{
+                    rows : 1,
+                },
+            },
+            1024:{
+                spaceBetween: 32,
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                grid:{
+                    rows : 1,
+                },
+            },
+            1280:{
+                spaceBetween: 26,
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+                grid:{
+                    rows : 1,
+                },
+            },
+        }
     })
 };
